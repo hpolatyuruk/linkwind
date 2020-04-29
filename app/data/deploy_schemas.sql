b67211f0e2c5e2c0e2f7a26b1b1e128d3a538bf4
hpolatyuruk <h.polatyuruk@gmail.com>
2020-04-29 15:20:32 +0200

    -Moved docker files to their own app folders -Minor changes

--- app/data/deploy_schemas.sql
+++ app/data/deploy_schemas.sql
@@ -419,3 +419,88 @@ TABLESPACE
 
                                     ALTER TABLE public.resetpasswordtokens
     OWNER to postgres;
+
+
+
+
+
+
+                                    CREATE OR REPLACE FUNCTION public.calculatestorypenalty
+                                    (
+	commentcount integer)
+    RETURNS integer
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    IMMUTABLE 
+AS $BODY$declare
+    penalty integer := 40;
+    i integer := -1;
+                                    begin 
+    loop
+        exit when penalty = i;
+				i := i+1;
+                                    if (commentCount = i) then
+                                    return penalty-i;
+                                    end
+                                    if;
+    end loop;
+                                    return 1;
+
+                                    end ;
+
+$BODY$;
+
+                                    ALTER FUNCTION public.calculatestorypenalty(integer)
+    OWNER TO postgres;
+
+
+
+
+
+
+                                    CREATE OR REPLACE FUNCTION public.calculatestoryrank
+                                    (
+	stories)
+    RETURNS double precision
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    IMMUTABLE 
+AS $BODY$declare
+
+    timeNow timestamp;
+
+    votes integer;
+
+    up integer;
+
+    down float;
+
+    timeDiff integer;
+
+                                    begin 
+    votes := $1.upvotes- $1.downvotes;
+                                    if (votes <= 0) then
+        votes := 1;
+                                    end
+                                    if;
+    up :=  POWER
+                                    ((votes),0.8);
+    timeNow := NOW
+                                    ();
+    timeDiff :=  EXTRACT
+                                    (EPOCH 
+                         FROM
+                                    (timeNow::timestamp - 
+                               $1.submittedon::timestamp));
+    down := POWER
+                                    (timeDiff+1,0.1);
+                                    return (up/down)*calculatestorypenalty($1.
+                                    commentcount);
+                                    end ;
+
+$BODY$;
+
+                                    ALTER FUNCTION public.calculatestoryrank(stories)
+    OWNER TO postgres;
--- db.prod.dockerfile
+++
@@ -1,3 +0,0 @@
-FROM postgres:latest
-
-COPY ./app/data/deploy_schemas.sql /docker-entrypoint-initdb.d/
